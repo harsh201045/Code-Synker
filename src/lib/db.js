0fe2107d47aee47352a350268e5c5d2f253fa324
Bhavy Ramani <69421006+bhavyramani@users.noreply.github.com>
2024-06-27 19:16:07 +0530

    Database Connection

---
+++ src/lib/db.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+let cached = {
+    conn: null,
+    promise: null,
+};
+
+export async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn;
+    } else {
+        const MONGO_URL = process.env.MONGO_URL;
+
+        if (!MONGO_URL) {
+            throw new Error('Please define the MONGO_URL environment variable inside .env.local');
+        }
+
+        const promise = mongoose.connect(MONGO_URL, {
+            autoIndex: true,
+        });
+
+        cached = {
+            conn: await promise,
+            promise,
+        }
+
+        console.log("Connected");
+        return await promise;
+    }
+}
