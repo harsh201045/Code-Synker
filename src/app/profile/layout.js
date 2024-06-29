ed6e0d7cdfb315d3536b288008fcb6b6802e43c2
harsh201045 <harshpabani2010@gmail.com>
2024-06-29 18:34:13 +0530

    sidebar changes

--- src/app/profile/layout.js
+++ src/app/profile/layout.js
@@ -1,22 +1,43 @@
+"use client"
 import { Inter } from "next/font/google";
 import '../globals.css';
+import nextAuth from "next-auth";
+import { useSession } from "next-auth/react";
+// import Head from "next/head";
+import { useEffect } from "react";
 
-export const metadata = {
-  title: "Profile Page",
-  description: "Sync Your code now!!!!",
-};
+
+// export const metadata = {
+//   title: "Profile Page",
+//   description: "Sync Your code now!!!!",
+// };
 
 // ProfileLayout component
 export default function ProfileLayout({ children }) {
+
+    const { data: session } = useSession();
+    useEffect(() => {
+        if (session) {
+          document.title = `${session.user.name} - Code-synker`;
+        }
+      }, [session]);
+
+    if (!session) {
+        return <div>Loading...</div>;
+    }
+    
+
   return (
+    
+    <>
     <div className="flex min-h-screen ">
       {/* Sidebar */}
       <aside className="w-64 h-screen px-4 py-8 overflow-y-auto bg-gray-800 border-r rtl:border-r-0 rtl:border-l dark:border-gray-700">
 
         <div className="flex flex-col items-center mt-6 -mx-2">
-          <img className="object-cover w-24 h-24 mx-2 rounded-full" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar" />
-          <h4 className="mx-2 mt-2 font-medium text-gray-200">John Doe</h4>
-          <p className="mx-2 mt-1 text-sm font-medium text-gray-400">john@example.com</p>
+          <img className="object-cover w-24 h-24 mx-2 rounded-full" src={session.user.image} alt="avatar" />
+          <h4 className="mx-2 mt-2 font-medium text-gray-200">{session.user.name}</h4>
+          <p className="mx-2 mt-1 text-sm font-medium text-gray-400">{session.user.email}</p>
         </div>
 
         <div className="flex flex-col justify-between flex-1 mt-6">
@@ -53,5 +74,6 @@ export default function ProfileLayout({ children }) {
         {children}
       </main>
     </div>
+    </>
   );
 }
